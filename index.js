9c64019c2ae4b58c91a231ed49d45e62c447557b
NiPh <niph@mail.ru>
2017-03-22 17:44:03 +0300

    Удаление пробела в комментарии на бэкенде

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server = http.createServer( function(req, res) {
 
 var closeCo = function (index) {
     return function () {
-        console.log('Минус один дракон ');
+        console.log('Минус один дракон');
         delete peers[index];
     }
 } 
