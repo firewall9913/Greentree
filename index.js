fba81eef4523c1e76a0015b7c62f65269727f209
Vladislav <freeman1813@gmail.com>
2017-04-14 23:48:35 +0300

    улучшен стиль, и доработан handshake

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ var WebSocketServer = require('ws').Server,
     index = 0;
 
 var nicker = function(post){
-  return function(){
-    nick += post.nick;
+    //post.nick, post.hash
+ console.log(post.nick, post.key);
+  for (var i in peers) {
+      console.log(peers[i].key);
+      if(peers[i].key === post.key){
+          console.log(true);
+        peers[i].nick = post.nick;
+      }
   }
 };
+
+function findNickByHash(hash) {
+    for (var i in peers) {
+      if(peers[i].key === hash){
+        return peers[i].nick;
+      }
+  }
+}
 
 server = http.createServer( function(req, res) {
   console.log(req.method);
@@ -23,21 +37,30 @@ server = http.createServer( function(req, res) {
     });
     req.on('end', function(){
       var post = qs.parse(body);
-      if (post.type = 'handshake'){
-        nicker(post);
-        console.log('Зарегистрирован '+nick);
-        res.end('registration completed');
-      }
-      if (post.message != undefined){
-        console.log(post);
-        if(post.message=='/stat'){
-          showStat();
-        } else{
-          broadcast(post);
+
+     
+        switch(post.type){
+            case 'handshake':
+                nicker(post);
+                console.log('Зарегистрирован '+nick);
+                res.end('registration completed');
+                break;
+                
+            default:
+                if (post.message != undefined){
+                    if(post.message=='/stat'){
+                          showStat();
+                        } else{
+                          post.nick = findNickByHash(post.key);    
+                          broadcast(post);
+                        }
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.end('post received');
+                  } 
+                break;
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
-      } 
+          
+
     });
   } else {res.end('post received')
   }
@@ -55,20 +78,20 @@ host = '192.168.1.12';
 server.listen(port, host);
 console.log('listening at http://' + host +':' + port);
 wss.on('connection', function(ws){
-  handshake(ws);
+  key = handshake(ws);
   index += 1;
   StrangeIp = ws._socket.remoteAddress;
   ip = StrangeIp.substr(7);
-  peers[index] = {ws:ws, ip:ip, nick:nick};
+  peers[index] = {ws:ws, ip:ip, key:key};
   console.log("новое соединение " + index);  
   ws.on('close', closeConn(index));
 });
 
 
 function broadcast(data){
-  for (var i = 1; i < peers.length; i++) {
+  for (var i in peers) {  
     if (peers[i] != undefined) {
-      send(peers.ws[i])
+      send(peers[i].ws, data)
     }
   };
 }
@@ -76,18 +99,19 @@ function broadcast(data){
 var showStat = function(){
   var post = {};
   post.nick = 'Websocket';
-  var stat = '';
-  for (var i = 1; i < peers.length; i++){
+  var stat = '<hr/>';
+  for (var i in peers){
     if (peers[i] != undefined) {
-      var stat = stat+'Соединение '+i+' - '+peers.ip[i]+' - '+peers.nick[i]+';/n  '
+      var stat = stat+'Соединение '+i+' - '+peers[i].ip+' - '+peers[i].nick+'  '
     }
   }
+  stat = stat+'<hr/>';
   post.message = stat;
   broadcast(post);
   stat = 'Соединение ';
 };
 
-var send = function (ws){
+var send = function (ws, data){
   ws.send (JSON.stringify (data));
 }
 
@@ -100,5 +124,5 @@ function handshake(ws){
                    .digest('base64');
   cache = {type:'handshake', key:Hash};
   ws.send (JSON.stringify (cache));
-  var something = '';
+  return Hash;
 }
